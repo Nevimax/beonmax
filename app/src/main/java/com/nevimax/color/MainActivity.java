788f45d894660ed301c5f4354caf1fc0a068c507
Nevimax <nevimax@gmail.com>
2020-08-06 00:50:47 +0200

    fourlesAndroid

--- app/src/main/java/com/nevimax/color/MainActivity.java
+++ app/src/main/java/com/nevimax/color/MainActivity.java
@@ -9,12 +9,25 @@ import android.widget.TextView;
 import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner spColors;
+    private TextView tvDescriptionTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spColors = findViewById(R.id.sp_Colors);
+        tvDescriptionTemp = findViewById(R.id.tv_DescriptionTemp);
+    }
 
+    public void onClickShowDescription(View view) {
+        int position = spColors.getSelectedItemPosition();
+        String description = getDescriptionByPosition(position);
+        tvDescriptionTemp.setText(description);
+    }
+
+    private String getDescriptionByPosition(int position) {
+        String[] descriptions = getResources().getStringArray(R.array.sp_description_of_temp);
+        return descriptions[position];
     }
 }
