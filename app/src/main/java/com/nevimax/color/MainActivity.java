d07f483b2ef7a06dc35886250dc6084aa96eda64
Nevimax <nevimax@gmail.com>
2020-08-06 00:45:20 +0200

    start

---
+++ app/src/main/java/com/nevimax/color/MainActivity.java
@@ -0,0 +1,20 @@
+package com.nevimax.color;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
